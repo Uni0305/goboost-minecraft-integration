d9f1e7b508149ba62a2d533aa1d3404afbb9efbe
Uni0305 <git@uni0305.me>
2023-12-10 19:13:44 +0900

    Feat(custom-event): Implement async scheduled task for polling notification api

--- src/main/java/me/uni0305/goboost/BoostIntegrationPlugin.java
+++ src/main/java/me/uni0305/goboost/BoostIntegrationPlugin.java
@@ -1,7 +1,13 @@
 package me.uni0305.goboost;
 
+import me.uni0305.goboost.api.BoostAPI;
+import me.uni0305.goboost.api.BoostNotificationData;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class BoostIntegrationPlugin extends JavaPlugin {
 
@@ -10,6 +16,22 @@ public class BoostIntegrationPlugin extends JavaPlugin {
         YamlConfigurator.saveDefaultConfig();
         YamlConfigurator.reloadConfig();
         new BoostEventListener(this);
+        schedulePollingTask();
+    }
+
+    private void schedulePollingTask() {
+        AtomicReference<String> cursor = new AtomicReference<>("");
+        getServer().getAsyncScheduler().runAtFixedRate(this, (task) -> BoostAPI.getEvents(cursor.get()).thenAccept((response) -> {
+            cursor.set(response.cursor());
+            List<BoostNotificationData> notifications = response.notifications();
+
+            if (notifications.isEmpty()) return;
+            boolean called = new AsyncReceiveBoostNotificationEvent(notifications).callEvent();
+            if (!called) getSLF4JLogger().error("An error occurred while calling the BoostNotificationEvent");
+        }).exceptionally((e) -> {
+            getSLF4JLogger().error("An error occurred while polling for boost notifications", e);
+            return null;
+        }), 0, 1, TimeUnit.SECONDS);
     }
 
     public static @NotNull JavaPlugin getPlugin() {
