2659ee3971197f3855e0cc14d16a255d1f6dc55e
Uni0305 <git@uni0305.me>
2023-12-10 21:14:48 +0900

    Fix: Fixed wrong jitpack jdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,9 +33,6 @@ tasks.runServer {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = project.group.toString()
-            artifactId = project.name
-            version = project.version.toString()
             from(components["java"])
         }
     }
