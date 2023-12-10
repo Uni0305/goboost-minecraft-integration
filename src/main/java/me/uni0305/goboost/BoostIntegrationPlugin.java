98eb61c7fe04ce0f3126a86d0580bb6c3b181f2e
Uni0305 <git@uni0305.me>
2023-12-10 20:17:47 +0900

    Style: Reformat codes

--- src/main/java/me/uni0305/goboost/BoostIntegrationPlugin.java
+++ src/main/java/me/uni0305/goboost/BoostIntegrationPlugin.java
@@ -4,15 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 public class BoostIntegrationPlugin extends JavaPlugin {
+    public static @NotNull JavaPlugin getPlugin() {
+        return getPlugin(BoostIntegrationPlugin.class);
+    }
 
     @Override
     public void onEnable() {
         YamlConfigurator.saveDefaultConfig();
         YamlConfigurator.reloadConfig();
         new BoostCommand(this);
-    }
-
-    public static @NotNull JavaPlugin getPlugin() {
-        return getPlugin(BoostIntegrationPlugin.class);
     }
 }
--- src/main/java/me/uni0305/goboost/api/BoostNotificationData.java
+++ src/main/java/me/uni0305/goboost/api/BoostNotificationData.java
@@ -5,14 +5,12 @@ import org.jetbrains.annotations.NotNull;
 
 import java.time.ZonedDateTime;
 
-public record BoostNotificationData(
-        @SerializedName("Id") @NotNull String id,
-        @SerializedName("UserId") @NotNull String userId,
-        @SerializedName("UserName") @NotNull String userName,
-        @SerializedName("Currency") @NotNull String currency,
-        @SerializedName("Amount") long amount,
-        @SerializedName("CreatedAt") @NotNull String createdAt
-) {
+public record BoostNotificationData(@SerializedName("Id") @NotNull String id,
+                                    @SerializedName("UserId") @NotNull String userId,
+                                    @SerializedName("UserName") @NotNull String userName,
+                                    @SerializedName("Currency") @NotNull String currency,
+                                    @SerializedName("Amount") long amount,
+                                    @SerializedName("CreatedAt") @NotNull String createdAt) {
     public @NotNull ZonedDateTime getCreatedDateTime() {
         return ZonedDateTime.parse(createdAt);
     }
